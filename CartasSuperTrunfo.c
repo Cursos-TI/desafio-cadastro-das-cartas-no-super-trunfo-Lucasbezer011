906850e841402fc1c135f0a4c9ce1ea65979b428
Lucasbezer011 <lucassb404@gmail.com>
2025-02-27 23:08:35 +0000

    novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,7 +19,23 @@ int main() {
         float area1, area2, pib1, pib2;
 
         int pontosTuristicos1, pontosTuristicos2;
-        // Cadastro das Cartas
+
+        float densidade1, densidade2;
+
+        float pibcapita1, pibcapita2;
+
+        // Calculo densidade/ Calculo PIB;
+
+        densidade1 = populacao1 / area1;
+
+        densidade2 = populacao2 / area2;
+
+        pibcapita1 = pib1 / populacao1;
+
+        pibcapita2 = pib2 / populacao2;
+
+
+      // Cadastro das Cartas
         // Cadastro carta 1
         // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
 
@@ -73,8 +89,10 @@ int main() {
             printf("Nome da Cidade: %s\n", cidade1);
             printf("População: %d\n", populacao1);
             printf("Área: %.2f km²\n", area1);
-            printf("PIB: %.2f bilhões de reais\n", pib1);
+            printf("PIB: %.2f\n", pib1);
             printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+            printf("Densidade populacional: %f\n", densidade1);
+            printf("PIB per capita: %f\n", pibcapita1);
 
             // Informações Carta 2:
 
@@ -84,8 +102,10 @@ int main() {
             printf("Nome da Cidade: %s\n", cidade2);
             printf("População: %d\n", populacao2);
             printf("Área: %.2f km²\n", area2);
-            printf("PIB: %.2f bilhões de reais\n", pib2);
+            printf("PIB: %.2f\n", pib2);
             printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+            printf("Densidade populacional: %f\n", densidade2);
+            printf("PIB per capita: %f\n", pibcapita2);
 
 
     
